bcb82b9480a01f30834f55ce97da7f5ebb0e57b8
Ryan Lynn Rosas <rltrosas88@gmail.com>
2021-06-09 13:01:18 -0500

    2.5 add mock for potion and refactor player constructor part 1

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,5 +1,12 @@
 //when requiring modules it is not necessary to include the .js file extension because
     //Node will assume that the file is a JS file if no extension is specified
+//imports the Potion() constructor into the test, establishing Potion as a usable variable.
+const Potion = require('../lib/Potion');
+//mocks or replaces the constructor's implementation with our faked data
+jest.mock('../lib/Potion');
+
+console.log(new Potion());
+
 const Player = require('../lib/Player');
 
 test('creates a player object', () => {
@@ -9,4 +16,8 @@ test('creates a player object', () => {
     expect(player.health).toEqual(expect.any(Number));
     expect(player.strength).toEqual(expect.any(Number));
     expect(player.agility).toEqual(expect.any(Number));
+
+    expect(player.inventory).toEqual(
+        expect.arrayContaining([expect.any(Object)])
+      );
   });
